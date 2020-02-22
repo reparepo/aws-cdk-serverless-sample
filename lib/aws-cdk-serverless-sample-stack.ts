91c5d773cda55ab5361f2abaea883e7c3737d219
Pahud <pahudnet@gmail.com>
2020-02-22 14:14:58 +0800

    first commit

--- lib/aws-cdk-serverless-sample-stack.ts
+++ lib/aws-cdk-serverless-sample-stack.ts
@@ -1,9 +1,22 @@
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 import * as cdk from '@aws-cdk/core';
 
 export class AwsCdkServerlessSampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const handler = new lambda.Function(this, 'MyFunc', {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      code: new lambda.InlineCode(`
+import json
+def handler(event, context):
+      return {
+        'statusCode': 200,
+        'body': json.dumps('Hello CDK from Lambda!')
+      }`),
+    });
 
-    // The code that defines your stack goes here
+    new apigateway.LambdaRestApi(this, 'API', { handler })
   }
 }
