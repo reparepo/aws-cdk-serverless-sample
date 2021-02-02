18497c9b1ce8e31f21ddb7c1ba2cc8cef9816924
Pahud Hsieh <pahudnet@gmail.com>
2021-02-02 03:11:00 +0000

    feat: add new workflow

--- .projenrc.js
+++ .projenrc.js
@@ -21,16 +21,53 @@ const project = new AwsCdkTypeScriptApp({
 });
 
 // create a custom projen and yarn upgrade workflow
-workflow = project.github.addWorkflow('ProjenYarnUpgrade');
+projenYarnUpgrade = project.github.addWorkflow('ProjenYarnUpgrade');
 
-workflow.on({
+projenYarnUpgrade.on({
   schedule: [{
     cron: '11 0 * * *',
   }], // 0:11am every day
   workflow_dispatch: {}, // allow manual triggering
 });
 
-workflow.addJobs({
+projenYarnUpgrade.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      { uses: 'actions/checkout@v2' },
+      {
+        uses: 'actions/setup-node@v1',
+        with: {
+          'node-version': '10.17.0',
+        },
+      },
+      { run: 'yarn upgrade' },
+      { run: 'yarn projen:upgrade' },
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+          'labels': 'auto-merge',
+        },
+      },
+    ],
+  },
+});
+
+// allow manual run this workflow to update the test snapshots
+projenYarnUpgradeUpdateTest = project.github.addWorkflow('ProjenYarnUpgradeUpdateTest');
+
+projenYarnUpgradeUpdateTest.on({
+  workflow_dispatch: {}, // manual trigger only
+});
+
+projenYarnUpgradeUpdateTest.addJobs({
   upgrade: {
     'runs-on': 'ubuntu-latest',
     'steps': [
